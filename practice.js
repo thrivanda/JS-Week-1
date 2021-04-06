e65d6d98a0b485c2b639a192dcf150a091b1a391
angellee <angel.purnamasari@aia.com>
2021-04-06 17:12:26 +0800

    add: 2 more practice test

--- practice.js
+++ practice.js
@@ -4,6 +4,7 @@
  */
 const highestGrade = (arrayStudentList) => {
     // please write your solution here
+    return arrayStudentList.filter(val => val.major == 'Cyber Security' && val.score >= 90)
 };
 
 const arrayStudentList = [
@@ -15,7 +16,7 @@ const arrayStudentList = [
     { name: "Yeremia", score: 92, major: "Cyber Security"},
 ]
 
-// test case 
+// test case for Highest Grade
 const testCaseGetHighestGrade = (testValue, expectedValue) => {
     for(let i = 0; i < testValue.length; i++) {
         console.log(testValue[i].name === expectedValue[i].name  && testValue[i].major === expectedValue[i].major  ? 'Get Highest Grade: Success!' : 'Get Highest Grade: Failed!');
@@ -26,3 +27,35 @@ testCaseGetHighestGrade(highestGrade(arrayStudentList), [ { name: 'Joanne', scor
 { name: 'Yeremia', score: 92, major: 'Cyber Security' } ]);
 
 
+/**
+ *  Shopping Cart - Sum the value if the value is same
+ * @param {Object[]} val  
+ */
+const shoppingCart = (val) => {
+    // please write your solution here
+}
+
+// test case for Shopping Cart
+const testShoppingCart = (testValue, expectedValue) => {
+   return console.log(JSON.stringify(testValue) === JSON.stringify(expectedValue) ? 'Shopping Cart: Success!' : 'Shopping Cart: Failed!');
+};
+
+testShoppingCart(shoppingCart(['coffe', 'egg', 'milk', 'egg', 'cookingOil', 'egg']), { coffe: 1, egg: 3, milk: 1, cookingOil: 1 });
+testShoppingCart(shoppingCart(['chocolate', 'noodles', 'chocolate']), { chocolate: 2, noodles: 1 });
+
+
+/**
+ * Capitalize Words - make every first letter word to capital
+ * @param {*} arrWords 
+ */
+const capitalizeWords = (arrWords) => {
+    // Please write your solution here
+}
+
+// test case for Capitalize Words
+const testCapitalizeWords = (testValue, expectedValue) => {
+    return console.log(testValue === expectedValue ? 'Capitalize Words: Success!' : 'Capitalize Words: Failed!');
+ };
+ 
+testCapitalizeWords(capitalizeWords('hElLO wORLd!'), 'Hello World!');
+testCapitalizeWords(capitalizeWords('the qUick BroWn Fox'), 'The Quick Brown Fox');
